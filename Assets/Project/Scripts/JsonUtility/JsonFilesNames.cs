5f8ab082fb3d7c07084800191f84674d9b8ad42e
Evgeniy Serdiuk <serdiuk.ev93@gmail.com>
2025-01-21 15:26:37 +0700

    add remote json loader

---
+++ Assets/Project/Scripts/JsonUtility/JsonFilesNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Scripts.JsonUtility
+{
+    public static class JsonFilesNames
+    {
+        public const string SETTINGS = "Settings.json";
+        public const string TEXTS = "Texts.json";
+    }
+}
---
+++ Assets/Project/Scripts/JsonUtility/RemoteJsonLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BundleTest.Project.Scripts.JsonUtility
+{
+    public class RemoteJsonLoader
+    {
+        private const string URL = "https://raw.githubusercontent.com/EvgeniySerdiuk/Json/main/";
+
+        public async UniTask<T> DownloadAsset<T>(string name, CancellationToken token)
+        {
+            try
+            { 
+                using (UnityWebRequest request = UnityWebRequest.Get(URL + name))
+                {
+                    await request.SendWebRequest().ToUniTask(cancellationToken: token);
+                    var json = request.downloadHandler.text;
+
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+
+            return default;
+        }
+    }
+}
